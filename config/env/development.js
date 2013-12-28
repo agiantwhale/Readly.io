114129124a3220dd4c06cfe264ab26062926203e
Il Jae <smashthewindow@gmail.com>
2013-12-29 00:09:23 +0900

    Added Mary Gibson viral account
    Lets watch this thing blow up now

--- config/env/development.js
+++ config/env/development.js
@@ -5,14 +5,21 @@ module.exports = {
     app: {
         name: "ReadAgain"
     },
+    mail: {
+        service: "Gmail",
+        auth: {
+            user: "mgibson095@gmail.com",
+            pass: "Of6v71X1R#&fHkW8MXJ.Z{i024pcO274Je7Z3pD}3E3NP$BlII"
+        }
+    },
     facebook: {
         clientID: "APP_ID",
         clientSecret: "APP_SECRET",
         callbackURL: "http://localhost:3000/auth/facebook/callback"
     },
     twitter: {
-        clientID: "5pPUdA7wN4fOjgpQyntw",
-        clientSecret: "rPrvoNVDj8CbwhTHqfZ94BnoFkwzRraxGyPWPxasE0",
+        clientID: "6edUCj3udKXFHjWT4VP0hQ",
+        clientSecret: "2Y7ZCCkrJeRyzBJUkn5VGqpORivnzzroqZucczIE4k",
         callbackURL: "http://localhost:3000/auth/twitter/callback"
     },
     github: {
