1d67c493f3a2c209d53a17f58f94f78a837c1f26
Il Jae Lee <smashthewindow@gmail.com>
2013-12-28 17:39:11 +0900

    Debugging...

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 exports.render = function(req, res) {
+    console.log('Hello World!');
+
     res.render('index', {
         user: req.user ? JSON.stringify(req.user) : 'null'
     });
